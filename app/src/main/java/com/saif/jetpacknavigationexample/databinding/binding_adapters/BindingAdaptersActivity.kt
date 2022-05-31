b768437a03df76eea7dcb045498ae6204c9691a3
Afnan7031 <afnan7031@gmail.com>
2022-05-31 09:36:31 +0500

    binding adapters

---
+++ app/src/main/java/com/saif/jetpacknavigationexample/databinding/binding_adapters/BindingAdaptersActivity.kt
@@ -0,0 +1,12 @@
+package com.saif.jetpacknavigationexample.databinding.binding_adapters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saif.jetpacknavigationexample.R
+
+class BindingAdaptersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_binding_adapters)
+    }
+}
