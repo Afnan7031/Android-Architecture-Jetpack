5c4200fd88d8f46543de3cd904a2fcd54ee04efe
Afnan7031 <afnan7031@gmail.com>
2022-06-01 23:37:54 +0500

    room

---
+++ app/src/main/java/com/saif/jetpacknavigationexample/databinding/binding_adapters/BindingAdaptersActivity.kt
@@ -0,0 +1,22 @@
+package com.saif.jetpacknavigationexample.databinding.binding_adapters
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.saif.jetpacknavigationexample.R
+import com.saif.jetpacknavigationexample.databinding.ActivityBindingAdaptersBinding
+
+class BindingAdaptersActivity : AppCompatActivity() {
+ private lateinit var binding : ActivityBindingAdaptersBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_binding_adapters)
+
+        val post = Post("Intro to Kotlin",
+            "Hey Welcome to FaniLabs",
+            "https://source.unsplash.com/user/c_v_r/100x100"
+         )
+        binding.post = post
+    }
+}
---
+++ app/src/main/java/com/saif/jetpacknavigationexample/databinding/binding_adapters/Post.kt
@@ -0,0 +1,3 @@
+package com.saif.jetpacknavigationexample.databinding.binding_adapters
+
+data class Post(val title: String, val description: String, val url: String)
